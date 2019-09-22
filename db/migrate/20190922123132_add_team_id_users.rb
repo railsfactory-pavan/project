8b2db47825d15792e9cc57f258f3bebf8f1f5c15
railsfactory-pavan <pavanrf07@gmail.com>
2019-09-22 18:59:53 +0530

    migration stuffs solve

--- db/migrate/20190922123132_add_team_id_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTeamIdUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :team_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190922123132) do
+ActiveRecord::Schema.define(version: 20190922132131) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190922123132) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.integer  "team_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
