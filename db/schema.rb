5ae0007bdec3b20a81d2fb92b777696e2fe18c7c
railsfactory-pavan <pavanrf07@gmail.com>
2019-09-22 17:59:14 +0530

    name added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190922113629) do
+ActiveRecord::Schema.define(version: 20190922122654) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190922113629) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
